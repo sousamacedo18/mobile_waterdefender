f513a157b29b372e78e99804d21c8e491e5706a2
sousamacedo18 <sousamacedo18@gmail.com>
2022-02-01 16:42:48 -0300

    iniciando o projeto

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useEffect,useState, useRef} from "react";
+import { Text, View } from 'react-native';
+import {css} from './assets/CSS/css'
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 export default function App() {
+  const [origin, setOrigin] = useState(null);
+  const [destination, setDestination] = useState(null);
+ async function getLocation(){
+  let { status } = await  Location.requestForegroundPermissionsAsync();
+
+  if (status !== 'granted') {
+    
+    console.log('Permission to access location was denied');
+    return;
+  }else{
+ let location= await Location.getCurrentPositionAsync({enableHighAccuracy: true});
+ console.log(location);
+ setOrigin({
+  latitude: location.coords.latitude,
+  longitude: location.coords.longitude,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+ })
+return;
+ 
+  }
+
+ }
+  useEffect(()=>{
+
+    getLocation();
+
+     
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={css.container}>
+       <MapView
+    initialRegion={origin}
+    showsUserLocation={true}
+    zoomEnabled={false}
+       style={css.map} >
+
+       </MapView>
+       <View 
+       style={css.search}
+
+       >
+       <Text>fadafdf</Text>
+
+       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ assets/CSS/css.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const css = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    map:{
+        height:'60%',
+        width:'100%'
+      
+    },
+    search:{
+        height:'40%',
+        width:"100%",
+        backgroundColor:'gray'
+    }
+   
+  });
+  export {css};
